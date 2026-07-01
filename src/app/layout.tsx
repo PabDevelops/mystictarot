13298931d37d66f1c0cbc3c286de957929929bff
Pablo <prodriguezrial@gmail.com>
2026-07-01 08:25:12 +0100

    chore: add crystal ball favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Mystic Tarot",
   description: "A sleek, interactive Tarot reading web application.",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🔮</text></svg>",
+  },
 };
 
 export default function RootLayout({
